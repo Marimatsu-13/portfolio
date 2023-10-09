47561850002289e32ebb62825f9a261a6b5f9430
marina melgar <marina.melgar13@gmail.com>
2023-10-09 22:23:12 +0200

    Fin page contact

--- marinatheme/footer.php
+++ marinatheme/footer.php
@@ -1,11 +1,5 @@
 <footer>
-    <?php
-    
-    wp_nav_menu([
-        'theme_location' => 'footer',
-        'container' => false,
-    ]);
-    ?>
+
     </footer>
    
 <?php wp_footer() ?>
--- marinatheme/header.php
+++ marinatheme/header.php
@@ -9,6 +9,16 @@
 <!-- Création de nav bar -->
 
 <nav id="menu">
+<?php
+        if ( function_exists( 'the_custom_logo' ) ) {
+            the_custom_logo();
+            
+        wp_nav_menu(array(
+            'theme_location' => 'header',
+            'menu_id' => 'Navigation', 
+            'container' => false,
+        ));}
+        ?>
 <div class="menu-toggle">
             <div class="bar1"></div>
             <div class="bar2"></div>
