944353770868d1d0609f022f67ee7dfa172018e6
marina melgar <marina.melgar13@gmail.com>
2023-10-17 23:17:00 +0200

    Avancé page de formation

--- marinatheme/footer.php
+++ marinatheme/footer.php
@@ -1,7 +1,11 @@
 <footer>
-
+<ul class="social_icon">
+    <li><a href="https://github.com/Marimatsu-13" target="_blank" ><ion-icon name="logo-github"></ion-icon></a></li>
+    <li><a href="https://www.linkedin.com/in/marina-melgar-b24b1b1ba/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+</ul>
     </footer>
-    
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <?php wp_footer() ?>
 </body>
 </html>
--- marinatheme/template-formation.php
+++ marinatheme/template-formation.php
@@ -1,19 +1,19 @@
+<?php
+/**
+ * Template Name: Formation
+ */
+?>
 <?php get_header(); ?>
 <div id="preloader"></div>
-<div class="main single">
+<div class="main page">
 <?php if (have_posts()) : ?>
 <?php while (have_posts()) : the_post(); ?>
 <div class="post">
-<h1 class="post-title"><?php the_title(); ?></h1>
 <div class="post-content">
-<div class="thumbnail">
-<?php if (has_post_thumbnail()) {
-    the_post_thumbnail();}
-?>
-</div>
-<div class="content">
+    <div class="timeline">
 <?php the_content(); ?>
-</div>
+    </div>
+
 </div>
 </div>
 <?php endwhile; ?>
