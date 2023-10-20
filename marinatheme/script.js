62d2db11bddc552f769b004b62809694328ae000
marina melgar <marina.melgar13@gmail.com>
2023-10-20 18:31:17 +0200

    Fin2

--- marinatheme/script.js
+++ marinatheme/script.js
@@ -4,7 +4,7 @@ let bar1 = document.querySelector(".bar1");
 let bar2 = document.querySelector(".bar2");
 let bar3 = document.querySelector(".bar3");
 let burger = document.querySelector(".menu-toggle");
-let menu = document.querySelector(".menu ul");
+let menu = document.querySelector("#menu ul");
 
 burger.addEventListener("click", ()=>{
 burger.classList.toggle("active");
